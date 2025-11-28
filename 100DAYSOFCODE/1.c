355dde327aeaac1c28ea3ae9c79f14fa45785427
vs6070 <vaibhavsingh157715@gmail.com>
2025-11-28 22:44:31 +0530

    Add program to calculate the sum of two numbers

---
+++ 100DAYSOFCODE/1.c
@@ -0,0 +1,10 @@
+// 1st day of #100 days code 
+// Question 1 : Write a program to input two numbers and display their sum.
+#include <stdio.h>
+int main(){
+    int a, b , sum ;
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("Sum: %d\n", sum);
+    return 0;
+}
